a1d9a43bfa1e942cf749402d263fcfa1741c1f52
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2015-11-24 12:19:36 -0800

    parse filing types, query for older versions of filings

--- edgar-watcher.js
+++ edgar-watcher.js
@@ -1,8 +1,23 @@
 
+var configPath = process.argv[2] || './config.json';
+
 var request = require('request'),
     fs = require('fs'),
     exec = require('child_process').exec,
-    Diff2Html = require('diff2html').Diff2Html;
+    Diff2Html = require('diff2html').Diff2Html,
+    pg = require('pg'),
+    cheerio = require('cheerio'),
+    config = require(configPath);
+
+function handleError(err) {
+    if (err) {
+        console.log('Error:');
+        console.log(err);
+        return true;
+    }
+
+    return false;
+}
 
 function multiGet(urls, cb) {
     var results = {},
@@ -53,12 +68,68 @@ function buildDiffHtml(body) {
         '</html>';
 }
 
+function typeFromTitle(title) {
+    var normTitle = title.toUpperCase().trim();
+
+    if (normTitle.indexOf('S-1') == 0) { return 'S-1'; }
+    if (normTitle.indexOf('CT ORDER') == 0) { return 'CT ORDER'; }
+    if (normTitle.indexOf('S-8') == 0) { return 'S-8'; }
+    if (normTitle.indexOf('EFFECT') == 0) { return 'EFFECT'; }
+    if (normTitle.indexOf('CERTNYS') == 0) { return 'CERTNYS'; }
+    if (normTitle.indexOf('FWP') == 0) { return 'FWP'; }
+    if (normTitle.indexOf('8-A12B') == 0) { return '8-A12B'; }
+    if (normTitle.indexOf('DRS') == 0) { return 'DRS'; }
+    if (normTitle.indexOf('D') == 0) { return 'D'; }
+
+    return 'UNKNOWN';
+}
+
+function findOlderVersionOfEntry(entry, feed, cb) {
+    var entryType = typeFromTitle(entry.title);
+
+    console.log(config.databaseUrl)
+    pg.connect(config.databaseUrl, function(err, client, done) {
+        if (handleError(err)) { return done(true); }
+
+        // slect all the entries because we don't have a way to filter on filing type
+        client.query('SELECT * from entries WHERE feed = $1 ORDER BY date DESC', [feed], function(err, result) {
+            if (handleError(err)) { return done(true); }
+
+            for (var i = 0; i < result.rows.length; ++i) {
+                var row = result.rows[i];
+
+                if (typeFromTitle(row.title) == entryType && row.link != entry.link) {
+                    // found the most recent previous filing of the same type
+                    console.log(row);
+                    cb(row);
+                    break;
+                }
+            }
+
+            done();
+        });
+    });
+}
+
 var url1 = 'https://www.sec.gov/Archives/edgar/data/1512673/000119312515343733/d937622ds1.htm',
     url2 = 'https://www.sec.gov/Archives/edgar/data/1512673/000119312515352937/d937622ds1a.htm';
 
-diffDocs(url1, url2, function(diff) {
-    var body = Diff2Html.getPrettyHtml(diff, { inputFormat: 'diff' }),
-        html = buildDiffHtml(body);
+var filing1 = 'https://www.sec.gov/Archives/edgar/data/1512673/000119312515378578/0001193125-15-378578-index.htm',
+    feed = 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001512673&type=&dateb=&owner=exclude&start=0&count=40&output=atom',
+    entry = {
+        'title': 'S-1/A - stuff...',
+        'link': 'https://www.sec.gov/Archives/edgar/data/1512673/000119312515378578/0001193125-15-378578-index.htm'
+    }
 
-    fs.writeFileSync('index.html', html);
+findOlderVersionOfEntry(entry, feed, function(oldEntry) {
+
 });
+
+
+
+// diffDocs(url1, url2, function(diff) {
+//     var body = Diff2Html.getPrettyHtml(diff, { inputFormat: 'diff' }),
+//         html = buildDiffHtml(body);
+
+//     fs.writeFileSync('index.html', html);
+// });
