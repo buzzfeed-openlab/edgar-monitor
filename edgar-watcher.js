c2fc248f6b246a0e14d62999c666c14726a67f73
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2015-11-25 17:13:11 -0800

    set watcher up as an output handler for puppy

--- edgar-watcher.js
+++ edgar-watcher.js
@@ -1,13 +1,38 @@
 
-var configPath = process.argv[2] || './config.json';
-
 var request = require('request'),
     fs = require('fs'),
     exec = require('child_process').exec,
     Diff2Html = require('diff2html').Diff2Html,
     pg = require('pg'),
-    cheerio = require('cheerio'),
-    config = require(configPath);
+    cheerio = require('cheerio');
+
+var EdgarWatcher = module.exports = function(emitter, config) {
+    emitter.on('new-entry', function(entry, feed) {
+        if (typeFromTitle(entry.title) != "S-1") {
+            return;
+        }
+
+        findOlderVersionOfEntry(entry, feed, config.databaseUrl, function(oldEntry) {
+
+            getDocLinkFromEntry(entry, function(newLink) {
+                getDocLinkFromEntry(oldEntry, function(oldLink) {
+
+                    var entryDoc = typeFromTitle(entry.title) + '-' + Date.parse(entry.date),
+                        oldEntryDoc = typeFromTitle(oldEntry.title) + '-' + Date.parse(oldEntry.date);
+
+                    diffDocs(oldLink, oldEntryDoc, newLink, entryDoc, function(diff) {
+                        var body = Diff2Html.getPrettyHtml(diff, { inputFormat: 'diff' }),
+                            html = buildDiffHtml(body);
+
+                        fs.writeFileSync(entry.guid + '.html', html);
+                    });
+
+                });
+            });
+
+        });
+    });
+}
 
 function handleError(err) {
     if (err) {
@@ -38,15 +63,15 @@ function multiGet(urls, cb) {
     }
 };
 
-function diffDocs(url1, url2, cb) {
-    exec('w3m -dump -cols 200 ' + url1 + ' > doc1.txt', function(err) {
+function diffDocs(url1, title1, url2, title2, cb) {
+    exec('w3m -dump -cols 200 ' + url1 + ' > "' + title1 + '"', function(err) {
         if (handleError(err)) { return; }
 
-        exec('w3m -dump -cols 200 ' + url2 + ' > doc2.txt', function(err) {
+        exec('w3m -dump -cols 200 ' + url2 + ' > "' + title2 + '"', function(err) {
             if (handleError(err)) { return; }
 
             var oneGigInBytes = 1073741824;
-            exec('git diff --ignore-all-space --no-index doc1.txt doc2.txt',
+            exec('git diff --ignore-all-space --no-index "' + title1 + '" "' + title2 + '"',
                 { maxBuffer: oneGigInBytes - 1 }, function(err, stdout) {
                     if (err && err.code != 1) {
                         return handleError(err);
@@ -100,12 +125,12 @@ function normalizeLink(link) {
         .replace(/^(www\.)/, "");
 }
 
-function findOlderVersionOfEntry(entry, feed, cb) {
+function findOlderVersionOfEntry(entry, feed, databaseUrl, cb) {
     var entryType = typeFromTitle(entry.title),
         entryDate = new Date(entry.date),
         entryLink = normalizeLink(entry.link);
 
-    pg.connect(config.databaseUrl, function(err, client, done) {
+    pg.connect(databaseUrl, function(err, client, done) {
         if (handleError(err)) { return done(client); }
 
         // slect all the entries because we don't have a way to filter on filing type
@@ -114,11 +139,11 @@ function findOlderVersionOfEntry(entry, feed, cb) {
 
             for (var i = 0; i < result.rows.length; ++i) {
                 var row = result.rows[i],
-                    rowTitleType = typeFromTitle(row.title),
+                    rowType = typeFromTitle(row.title),
                     rowDate = new Date(row.date),
                     rowLink = normalizeLink(row.link);
 
-                if (rowTitleType == entryType && rowDate < entryDate && rowLink != entryLink) {
+                if (rowType == entryType && rowDate < entryDate && rowLink != entryLink) {
                     // found the most recent previous filing of the same type
                     cb(row);
                     break;
@@ -146,24 +171,3 @@ function getDocLinkFromEntry(entry, cb) {
         cb(docLink);
     });
 }
-
-var feed = 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001512673&type=&dateb=&owner=exclude&start=0&count=40&output=atom',
-    entry = {
-        'title': 'S-1/A - stuff...',
-        'link': 'https://www.sec.gov/Archives/edgar/data/1512673/000119312515352937/0001193125-15-352937-index.htm',
-        'date': 'Mon Oct 26 2015 09:01:50 GMT-0700 (PDT)'
-    };
-
-findOlderVersionOfEntry(entry, feed, function(oldEntry) {
-    getDocLinkFromEntry(entry, function(newLink) {
-        getDocLinkFromEntry(oldEntry, function(oldLink) {
-
-            diffDocs(oldLink, newLink, function(diff) {
-                var body = Diff2Html.getPrettyHtml(diff, { inputFormat: 'diff' }),
-                    html = buildDiffHtml(body);
-
-                fs.writeFileSync('index.html', html);
-            });
-        });
-    });
-});
